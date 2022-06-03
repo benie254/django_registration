0c50bbaff2cee9171aba220189d5f31cdb3e10d6
Benie <davinci.monalissa@gmail.com>
2022-06-03 04:24:54 +0300

    create a tester view function

--- reg/views.py
+++ reg/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+# from .email import send_email
+# from reg.forms import SubscriberForm
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def an_existing_view_function(request):
+	message = "Testing view function--hello!"
+
+	return render(request, 'index.html', {"message": message})
